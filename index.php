fb2c3ce57a485baa353cd2a7c75b13fd4ec93be8
Arthur Felippe <88401848+Thur17@users.noreply.github.com>
2025-01-21 23:14:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<div>
+    <b>
+    <p>
+        Notas
+    </b>
+    <br>
+        <?php
+
+// Ecercicio 
+
+$notas_dos_alunos = ['7.4', '1.7', '8.5', '3.5', '4.4', '8.7', '6.4', '8.4', '1.2', '4.3', '9.8', '0.5', '8.2',
+'4.7', '1.1', '3.3', '3.4', '4.8', '8.7', '5.4', '2.2', '3.7', '5.9', '7.4', '4.8', '4.7', '1.5', '8.4', '2.1', '2.7'];
+
+$aprovados = 0;
+$reprovados = 0; 
+
+foreach ($notas_dos_alunos as $notas){
+    if($notas >= 6 ){
+        $notas = "aprovados";
+        $aprovados = $aprovados +1;
+    }else 
+        $notas = "Reprovados";
+        $reprovados = $reprovados +1; 
+        echo "<p> $notas </P>";
+        
+}
+
+echo " Existem " .  $aprovados .  " aprovados e " . $reprovados . " reprovados ";
+
+?>
+        <b>
+    </p>
+</div>
+
+
+
+
